89c60d4dc65beefce1bc43b86a7a38cc2bbf8c1c
Jiadong.Yu <jiadong@pandu.ie>
2018-04-11 12:14:06 +0800

    add the sever and data

---
+++ server/index.js
@@ -0,0 +1,39 @@
+let express = require('express')
+let appData = require('./data.json')
+
+let apiRoutes = express.Router()
+
+let seller = appData.seller
+let goods = appData.goods
+let ratings = appData.ratings
+
+let app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+apiRoutes.get('/seller', function(req, res){
+  res.json({
+    errno: 0,
+    data: seller,
+  })
+});
+
+apiRoutes.get('/goods', function(req, res){
+  res.json({
+    errno: 0,
+    data: goods,
+  })
+});
+
+apiRoutes.get('/ratings', function(req, res){
+  res.json({
+    errno: 0,
+    data: ratings,
+  })
+});
+
+app.use('/api',apiRoutes)
+
+app.listen(9988);
