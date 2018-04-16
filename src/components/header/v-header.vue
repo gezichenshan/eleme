6819d7505ebcb508fd508d166af79987501b874c
Jiadong.Yu <jiadong@pandu.ie>
2018-04-16 10:10:58 +0800

    the pictures

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -13,18 +13,46 @@
               {{seller.description}}/{{seller.deliveryPrice}}分钟到达
             </div>
             <div v-if="seller.supports" class="support">
-              <span class="icon"></span>
+              <span class="icon" :class="classMap[seller.supports[0].type]"></span>
               <span class="text">{{seller.supports[0].description}}</span>
             </div>
           </div>
+          <div v-if="seller.supports" class="support-count" @click="showDetail">
+            <span class="count">
+              {{seller.supports.length}}个
+              <i class="icon-keyboard_arrow_right"></i>
+            </span>
+          </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+         <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+         <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+          <img :src="seller.avatar" width="100%" height="100%">
+        </div>
+        <div v-show="detailShow" class="detail">
+
+        </div>
     </div>
 </template>
 
 <script type="text/javascript">
 export default {
   props: ['seller'],
+  data() {
+    return {
+      detailShow: false,
+    }
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true
+    },
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
 }
 </script>
 
@@ -32,15 +60,22 @@ export default {
 @import '../../common/stylus/mixin.styl';
 
 .header {
+  position: relative;
   color: #fff;
-  background: #000;
+  background: rgba(7, 17, 27, 0.5);
 
   .content-wrapper {
+    position: relative;
     padding: 24px 12px 18px 24px;
     font-size: 0;
 
     .avatar {
       display: inline-block;
+      vertical-align: top;
+
+      img {
+        border-radius: 4px;
+      }
     }
 
     .content {
@@ -68,7 +103,129 @@ export default {
           font-weight: bold;
         }
       }
+
+      .description {
+        margin-bottom: 10px;
+        line-height: 12px;
+        font-size: 12px;
+      }
+
+      .support {
+        .icon {
+          display: inline-block;
+          vertical-align: top;
+          width: 12px;
+          height: 12px;
+          margin-right: 4px;
+          margin-top: 2px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+
+          &.decrease {
+            bg-img('decrease_1');
+          }
+
+          &.discount {
+            bg-img('discount_1');
+          }
+
+          &.guarantee {
+            bg-img('guarantee_1');
+          }
+
+          &.invoice {
+            bg-img('invoice_1');
+          }
+
+          &.special {
+            bg-img('special_1');
+          }
+        }
+
+        .text {
+          line-height: 12px;
+          font-size: 10px;
+        }
+      }
     }
+
+    .support-count {
+      position: absolute;
+      right: 12px;
+      bottom: 14px;
+      padding: 0 8px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 14px;
+      background: rgba(0, 0, 0, 0.2);
+      text-align: center;
+
+      .count {
+        vertical-align: top;
+        font-size: 10px;
+      }
+
+      .icon-keyboard_arrow_right {
+        margin-left: 2px;
+        line-height: 24px;
+        font-size: 10px;
+      }
+    }
+  }
+
+  .bulletin-wrapper {
+    position: relative;
+    height: 28px;
+    line-height: 28px;
+    padding: 0 22px 0 12px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    background: rgba(7, 17, 27, 0.2);
+
+    .bulletin-title {
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 10px;
+      width: 22px;
+      height: 12px;
+      bg-img('bulletin');
+      background-size: 22px 12px;
+      background-repeat: no-repeat;
+    }
+
+    .bulletin-text {
+      margin: 0 4px;
+      font-size: 10px;
+    }
+
+    .icon-keyboard_arrow_right {
+      position: absolute;
+      font-size: 10px;
+      right: 12px;
+      top: 10px;
+    }
+  }
+
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    filter: blur(10px);
+  }
+
+  .detail {
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background: rgba(7, 17, 27, 0.8);
   }
 }
 </style>
