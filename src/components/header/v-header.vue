d26bb2f3300ae51c04ae30f53a794dce8a8cc293
Jiadong.Yu <jiadong@pandu.ie>
2018-04-17 23:16:51 +0800

    star component

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -32,14 +32,31 @@
           <img :src="seller.avatar" width="100%" height="100%">
         </div>
         <div v-show="detailShow" class="detail">
-
+          <div class="detail-wrapper clearfix">
+            <div class="detail-main">
+              <h1 class="name">{{seller.name}}</h1>
+              <div class="star-wrapper">
+                <star :size="48" :score="seller.score"></star>
+              </div>
+              <div class="title">
+                <div class="line"></div>
+                <div class="text">优惠信息</div>
+                <div class="line"></div>
+              </div>
+            </div>
+          </div>
+          <div class="detail-close">
+            <i class="icon-close"></i>
+          </div>
         </div>
     </div>
 </template>
 
 <script type="text/javascript">
+import star from '@/components/star/'
 export default {
   props: ['seller'],
+  components: { star },
   data() {
     return {
       detailShow: false,
@@ -226,6 +243,56 @@ export default {
     height: 100%;
     overflow: auto;
     background: rgba(7, 17, 27, 0.8);
+
+    .detail-wrapper {
+      width: 100%;
+      min-height: 100%;
+
+      .detail-main {
+        margin-top: 64px;
+        padding-bottom: 64px;
+
+        .name {
+          line-height: 16px;
+          text-align: center;
+          font-size: 16px;
+          font-weight: 700;
+        }
+
+        .star-wrapper {
+          margin-top: 18px;
+          padding: 2px 0;
+          text-align: center;
+        }
+
+        .title {
+          display: flex;
+          width: 80%;
+          margin: 30px auto 24px auto;
+
+          .line {
+            flex: 1;
+            position: relative;
+            top: -6px;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+          }
+
+          .text {
+            padding: 0 12px;
+            font-size: 14px;
+          }
+        }
+      }
+    }
+
+    .detail-close {
+      position: relative;
+      width: 32px;
+      height: 32px;
+      margin: -64px auto 0 auto;
+      clear: both;
+      font-size: 32px;
+    }
   }
 }
 </style>
